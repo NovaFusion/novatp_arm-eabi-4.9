2f7c0e5736edb02629fd17841ba1ad868541889d
Marcin Chojnacki <marcinch7@gmail.com>
2014-04-19 20:56:10 +0200

    gcc-4.9-20140416

---
+++ lib/gcc/arm-eabi/4.9.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9-20140416/configure --target=arm-eabi --prefix=/home/marcin/toolchain/out --disable-nls --enable-languages=c --without-headers --with-arch=armv7-a --with-cpu=cortex-a9 --with-fpu=neon --with-float=hard";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "cortex-a9" }, { "arch", "armv7-a" }, { "float", "hard" }, { "fpu", "neon" } };
